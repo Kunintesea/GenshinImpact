51e2863d0c6e3dfce8364d7f6e985dcafa9578dc
Betagoe <2353604@tongji.edu.cn>
2024-12-03 14:11:41 +0800

    修复了相机移动的问题
    仅修改了相机的文件
    1.修改了相机移动的bug
    2.增加了两种跟随模式的切换开关（ .h文件里的const bool isFollowedDynamicly）

--- Genshin/Classes/newCamera.cpp
+++ Genshin/Classes/newCamera.cpp
@@ -21,6 +21,7 @@ bool newCamera::init()
 	camera = Camera::createPerspective(60, visibleSize.width / visibleSize.height, 1, 1000);
 	camera->setCameraFlag(CameraFlag::USER1);
 	cameraMove = Vec2::ZERO;
+	cameraBackMove = Vec2::ZERO;
 	return true;
 }
 
@@ -28,7 +29,7 @@ void newCamera::bindPlayer(Player* player)
 {
 	this->player = player;
 	camera->setPosition(player->getBody()->getPosition());
-	maxOffSet = 0.25 * 60 * player->getSpeed();
+	maxOffSet = 0.2 * 60 * player->getSpeed();
 }
 
 void newCamera::update(float dt)
@@ -39,7 +40,19 @@ void newCamera::update(float dt)
 	auto up = EventKeyboard::KeyCode::KEY_UP_ARROW;//上键
 	auto down = EventKeyboard::KeyCode::KEY_DOWN_ARROW;//下键
 
-	if (player->getKeyBoardState(left) && player->getKeyBoardState(up)) // 左上
+	if(player->getKeyBoardState(left) && player->getKeyBoardState(up) && player->getKeyBoardState(down))
+	      moveSet(-1, 0, dt);
+	else if(player->getKeyBoardState(right) && player->getKeyBoardState(up) && player->getKeyBoardState(down))
+	      moveSet(1, 0, dt);
+	else if(player->getKeyBoardState(left) && player->getKeyBoardState(right) && player->getKeyBoardState(up))
+	      moveSet(0, 1, dt);
+	else if(player->getKeyBoardState(left) && player->getKeyBoardState(right) && player->getKeyBoardState(down))
+	      moveSet(0, -1, dt);
+	// 如果左右按键同时按下，返回
+	else if (player->getKeyBoardState(left) && player->getKeyBoardState(right) // 左右
+	     || player->getKeyBoardState(down) && player->getKeyBoardState(up)) // 上下
+	      return;
+	else if (player->getKeyBoardState(left) && player->getKeyBoardState(up)) // 左上
 	      moveSet(-1, 1, dt);
 	else if (player->getKeyBoardState(left) && player->getKeyBoardState(down)) // 左下
 	      moveSet(-1, -1, dt);
@@ -55,32 +68,61 @@ void newCamera::update(float dt)
 	      moveSet(0, 1, dt);
 	else if (player->getKeyBoardState(down)) // 如果下键按下
 	      moveSet(0, -1, dt);
-	else 
-	      if (cameraMove != Vec2::ZERO &&
-		    (abs(cameraMove.x) > float(maxOffSet) || abs(cameraMove.y) > float(maxOffSet / 2)))
+	else if (!isFollowedDynamicly)
+	      return;
+	 else if (cameraMove != Vec2::ZERO &&
+	      (abs(cameraMove.x) > float(maxOffSet) || abs(cameraMove.y) > float(maxOffSet / 2)))
 	      {
-		    auto camera = Director::getInstance()->getRunningScene()->getDefaultCamera();
-		    MoveBy* move = MoveBy::create(0.3, cameraMove);
-		    camera->runAction(move);
+		    cameraBackMove += cameraMove;
 		    cameraMove = Vec2::ZERO;
 	      }
+
+	if (cameraBackMove != Vec2::ZERO){
+	      auto camera = Director::getInstance()->getRunningScene()->getDefaultCamera();
+	      float speed = player->getSpeed();
+
+	      if (cameraBackMove.x == 0 || cameraBackMove.y == 0)
+	      {
+		    Vec2 temp = cameraBackMove * cameraOffsetTime * 0.25;
+		    camera->setPosition(camera->getPosition() + temp);
+		    cameraBackMove -= temp;
+	      }
+	      else {
+		    Vec2 temp = cameraBackMove * cameraOffsetTime * 0.25 * 0.707;
+		    camera->setPosition(camera->getPosition() + temp);
+		    cameraBackMove -= temp;
+	      }
+	}
 }
 // 设置个方向的移动
 void newCamera::moveSet(int x, int y, float delta) {
+      //float deltaMove;
+      //if (x == 0 || y == 0) deltaMove = 1;
+      //else deltaMove = 0.707;
+
       float speed = player->getSpeed();
       auto camera = Director::getInstance()->getRunningScene()->getDefaultCamera();
       camera->setPosition(camera->getPosition() + Vec2(x, y) * speed);
 
+      if (!isFollowedDynamicly)
+	    return;
+
+      //camera->setPosition(camera->getPosition() + Vec2(x * deltaMove, y * deltaMove) * speed);
+
       if (abs(cameraMove.x) <= float(maxOffSet))
       {
 	    // 相机偏移
+	    // camera->setPosition(camera->getPosition() - Vec2(x * deltaMove * speed, 0));
+	    // cameraMove.x += x * deltaMove * speed;
 	    camera->setPosition(camera->getPosition() - Vec2(x * speed, 0));
 	    cameraMove.x += x * speed;
       }
       if (abs(cameraMove.y) <= float(maxOffSet) / 2)
       {
 	    // 相机偏移
+	    // camera->setPosition(camera->getPosition() - Vec2(0, y * deltaMove * speed));
 	    camera->setPosition(camera->getPosition() - Vec2(0, y * speed));
+	    // cameraMove.y += y * deltaMove * speed;
 	    cameraMove.y += y * speed;
       }
 }
